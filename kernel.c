4943a10feac0c18e396ac79ba5b6d38f40348825
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-09-14 16:43:26 +0100

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -14,7 +14,7 @@ void vline(int x,int y,int y1,char b)
 	int yyy=y1-y;
 	int xxxx=0;
 	int c=0;
-	if (y1>=y && x<320 && x>-1 && y>-1 && y<200)
+	if (y1>=y && x<320 && x>-1 && y>-1 && y<200  && y1>-1 && y1<200)
 	{
 		
 			   
@@ -30,7 +30,7 @@ void hline(int x,int y,int x1,char b)
 	int xxx=x1-x;
 	int xxxx=0;
 	int c=0;
-	if (x1>=x && x<320 && x>-1 && y>-1 && y<200)
+	if (x1>=x && x<320 && x>-1 && y>-1 && y<200 && x1<320 && x1>-1)
 	{
 		
 			   
@@ -38,6 +38,15 @@ void hline(int x,int y,int x1,char b)
 			   	pixels(b,i+c+xx);
 	}
 	
+}
+void box(int x,int y,int x1,int y1,char b)
+{
+	int n=0;
+	int nn=y1-y;
+	if (y1>=y && x1>=x && x<320 && x>-1 && y>-1 && y<200 && x1<320 && x1>-1 && y1>-1 && y1<200){
+	for (n=0;n<nn;n=n+1)   
+			hline(x,n+y,x1,0);
+			}
 }
  void cls()
  {
@@ -56,7 +65,8 @@ void hline(int x,int y,int x1,char b)
 		for (n=0;n<200;n=n+8)   
 			hline(0,n,319,0);
 		for (n=0;n<320;n=n+8)   
-			vline(n,0,199,0);	   	
+			vline(n,0,199,0);
+		box(150,75,175,100,0);	   	
 			   
 		return 0;	  
         }
